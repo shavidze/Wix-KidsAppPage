4e8b42ac3339a70b9c03938fafd3c9b990116064
ishav <iago.shavidze@wandio.com>
2019-04-04 17:22:59 +0400

    Implemented Smooth Image Animation With Jquery dom manipulation and requestAnimationFrame

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  function awaiter() {
+    setTimeout(function() {
+      var el = document.getElementsByClassName("turn");
+      console.log(el[0].className);
+      el[0].className = el[0].className.split(" ")[0];
+      document.getElementsByClassName("second")[0].className += " turn";
+    }, 3 * 1000);
+
+    setTimeout(function() {
+      var el = document.getElementsByClassName("turn");
+      console.log(el[0].className);
+
+      el[0].className = el[0].className.split(" ")[0];
+
+      document.getElementsByClassName("third")[0].className += " turn";
+    }, 2 * 3 * 1000);
+    setTimeout(function() {
+      var el = document.getElementsByClassName("turn");
+      console.log(el[0].className);
+
+      el[0].className = el[0].className.split(" ")[0];
+      document.getElementsByClassName("first")[0].className += " turn";
+
+      requestAnimationFrame(awaiter);
+    }, 3 * 3 * 1000);
+  }
+  requestAnimationFrame(awaiter);
+});
