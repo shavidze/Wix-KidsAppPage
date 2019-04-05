e7ba17dc4da6223382b4977afcfafa8ff48eb384
ishav <iago.shavidze@wandio.com>
2019-04-05 15:39:18 +0400

    Added main-content text section

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ $(document).ready(function() {
   function awaiter() {
     setTimeout(function() {
       var el = document.getElementsByClassName("turn");
-      console.log(el[0].className);
+      // console.log(el[0].className);
       el[0].className = el[0].className.split(" ")[0];
       document.getElementsByClassName("second")[0].className += " turn";
     }, 3 * 1000);
 
     setTimeout(function() {
       var el = document.getElementsByClassName("turn");
-      console.log(el[0].className);
+      // console.log(el[0].className);
 
       el[0].className = el[0].className.split(" ")[0];
 
       document.getElementsByClassName("third")[0].className += " turn";
     }, 2 * 3 * 1000);
+    
     setTimeout(function() {
       var el = document.getElementsByClassName("turn");
-      console.log(el[0].className);
+      // console.log(el[0].className);
 
       el[0].className = el[0].className.split(" ")[0];
       document.getElementsByClassName("first")[0].className += " turn";
